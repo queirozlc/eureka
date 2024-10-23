19a3dce11218b85fa107f652ca4a8b49bab0452c
caslu <sepulchrolucas@gmail.com>
2024-10-23 11:25:08 -0300

    add ci action workflow and tools for static code analysis

--- mix.exs
+++ mix.exs
@@ -60,7 +60,9 @@ defmodule Eureka.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:secure_random, "~> 0.5"}
+      {:secure_random, "~> 0.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
     ]
   end
 
