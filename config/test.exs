19b3d9f2d4512f501fe6d86044da08dea0ba9730
caslu <sepulchrolucas@gmail.com>
2024-11-12 15:35:03 -0300

    add secret config and request mock config for tests

--- config/test.exs
+++ config/test.exs
@@ -16,6 +16,11 @@ config :eureka, Eureka.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
+config :eureka,
+  eureka_req_options: [
+    plug: {Req.Test, Eureka}
+  ]
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :eureka, EurekaWeb.Endpoint,
