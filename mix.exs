a127e632a24f9889b42bbc3e9478c1fd5a2c499e
caslu <sepulchrolucas@gmail.com>
2024-11-12 15:33:43 -0300

    add `observer` as extra application

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Eureka.MixProject do
   def application do
     [
       mod: {Eureka.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :observer, :wx]
     ]
   end
 
